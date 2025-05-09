071ba8f0dbeb9f2f9bba296eb36e2eefa633584d
Mayderson Mello <maydersonmello@gmail.com>
2025-05-09 18:44:15 -0300

    call webhook to trigger workflow when final video is uploaded to cloudinary

--- upload-video.mjs
+++ upload-video.mjs
@@ -6,17 +6,38 @@ cloudinary.config({
   api_secret: process?.env?.CLOUDINARY_API_SECRET,
 });
 
-async function uploadVideoToCloudinary() {
+async function main() {
   console.log("\nUPLOADING VIDEO TO CLOUDINARY");
 
-  const response = await cloudinary.uploader.upload("out/rendered-video.mp4", {
-    folder: "bot/templates/instagram",
-    public_id: "rendered-video",
-    resource_type: "video",
-    format: "mp4",
-  });
+  const uploadResult = await cloudinary.uploader.upload(
+    "out/rendered-video.mp4",
+    {
+      folder: "bot/templates/instagram",
+      public_id: "rendered-video",
+      resource_type: "video",
+      format: "mp4",
+    },
+  );
 
-  console.log("\nVIDEO SENT SUCCESSFULLY: ", response?.secure_url);
+  console.log("\nVIDEO SENT SUCCESSFULLY");
+  console.log("\nCALL WEBHOOK TO TRIGGER WORKFLOW");
+
+  const webhookRequest = await fetch(process.env.WEBHOOK_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      videoWithSubtitles: uploadResult?.secure_url,
+    }),
+  });
+  const webhookResponse = await webhookRequest.json();
+
+  if (!webhookRequest.ok) {
+    console.error("\nError calling webhook: ", webhookResponse);
+  }
+
+  console.log("\nSUCCESSFULLY TRIGGERED WORKFLOW");
 }
 
-uploadVideoToCloudinary();
+main();
